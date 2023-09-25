216c750e49c68feef8ddb817acf6f4d7d23f14e4
Anthony Pageau <antopageau@hotmail.com>
2023-09-25 10:51:16 -0400

    user loging working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
 
+  resources :user_sessions, only: [:new, :create]
+  resources :users, only: [:index, :new, :create]
+
   resources :genres
   resources :songs
   resources :sub_genres
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_024251) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_135508) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "country"
@@ -56,6 +56,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_024251) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_sub_genres_on_genre_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "password_confirmation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "songs", "artists"
